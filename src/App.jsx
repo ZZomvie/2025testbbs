ed4522bee9e3515c03c98fdade86bacbf6bd1ff0
jhr88 <uchung22@gmail.com>
2025-10-24 15:51:42 +0900

    2nd test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import supabase from "./utills/supabase";
 
 function App() {
-  return <div>App</div>;
-}
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function getPosts() {
+      const { data: posts } = await supabase.from("posts").select();
+
+      setPosts(posts);
+    }
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h3>post</h3>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            {post.title} / {post.name} / {post.content}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
